5aa19bcf88f5cff1399095637a55ed419bc01936
unknown <anandmanash321@gmail.com>
2024-02-11 12:10:12 +0530

    added role based auth for both client and server pages

--- app/Member/page.jsx
+++ app/Member/page.jsx
@@ -1,9 +1,18 @@
+import { getServerSession } from 'next-auth'
 import React from 'react'
+import { options } from '../api/auth/[...nextauth]/options'
+import { redirect } from 'next/navigation'
 
-const Memeber = () => {
+const Memeber = async () => {
+  const session  = await getServerSession(options)
+  // console.log(session)
+  
+  if(!session) redirect("api/auth/signin?callbackUrl=/Member")
   return (
     <>
         It's a user based page coming from server 
+        {session && <p>{session?.user?.email}</p>}
+        {session && <p>{session?.user?.role}</p>}
     </>
   )
 }
