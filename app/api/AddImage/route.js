cd734cd7e0a6beffa9cc0c8b509e8042c71f8642
unknown <anandmanash321@gmail.com>
2024-02-23 11:58:06 +0530

    s3 file upload as trimmed name

--- app/api/AddImage/route.js
+++ app/api/AddImage/route.js
@@ -9,8 +9,9 @@ const s3 = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
 });
 
-async function uploadFileToS3(file,fileName){
+async function uploadFileToS3(file, fileName) {
   const fileBuffer = file;
+   fileName = fileName.trim();
   console.log(fileName);
 
   const params = {
@@ -18,14 +19,17 @@ async function uploadFileToS3(file,fileName){
     Key: `${fileName}-${Date.now()}`,
     // key: `myfolder/${fileName}-${Date.now()}`,
     Body: fileBuffer,
-    ContentType: "image/jpg"
-  }
+    ContentType: "image/jpg",
+  };
 
   const command = new PutObjectCommand(params);
-  const fileData =  await s3.send(command);
-  console.log(fileData)
-  return fileName;
+  const fileData = await s3.send(command);
 
+  const fileKey = params.Key;
+  const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.${process.env.AWS_BUCKET_REGION}.amazonaws.com/${fileKey}`;
+
+  console.log(fileUrl);
+  return fileName;
 }
 
 export async function POST(req) {
@@ -40,9 +44,12 @@ export async function POST(req) {
 
     const buffer = Buffer.from(await file.arrayBuffer());
 
-    const fileName = await uploadFileToS3(buffer,file.name);
+    const TrimmedName0fFile = file.name.replace(/\s+/g, '');
+    console.log(TrimmedName0fFile);
 
-    return NextResponse.json({success:true,fileName},{status:200});
+    const fileName = await uploadFileToS3(buffer, TrimmedName0fFile);
+
+    return NextResponse.json({ success: true, fileName }, { status: 200 });
   } catch (error) {
     console.log(error);
     return NextResponse.json({ message: "Error", error }, { status: 500 });
